117fa41f99f77abc31d02cfd96ec90bb8e309bb3
Fred Liu <liuxiaokun0410@qq.com>
2016-12-16 14:06:13 +0800

    init reflect tips project

---
+++ src/main/java/com/fred/tips/App.java
@@ -0,0 +1,13 @@
+package com.fred.tips;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
